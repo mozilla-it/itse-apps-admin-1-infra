4fe8e3e25c3b8f17cbdb21cd58d4b01c5c1f3815
Christina Harlow <charlow@mozilla.com>
2021-10-05 12:44:03 -0400

    remove tf for migrated or deleted AWS infra

--- terraform/state.tf
+++ terraform/state.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "itsre-state-783633885093"
-    key    = "us-west-2/itse-apps-admin-1/terraform.tfstate"
-    region = "eu-west-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "itsre-state-783633885093"
+#     key    = "us-west-2/itse-apps-admin-1/terraform.tfstate"
+#     region = "eu-west-1"
+#   }
+# }
